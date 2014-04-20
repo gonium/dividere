decfc2c2ea743d2e2c04d6ed2eeaadd90ad95c62
Mathias Dalheimer <md@gonium.net>
2014-04-20 17:26:58 +0200

    displaying w/ templates test code

--- dividere.go
+++ dividere.go
@@ -3,16 +3,17 @@ package main
 import (
 	"fmt"
 	"html/template"
-	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	pfp "path/filepath"
+  "github.com/dchest/uniuri"
 )
 
 var uploadTemplate, _ = template.ParseFiles("html/upload.html")
 var errorTemplate, _ = template.ParseFiles("html/error.html")
+var showTemplate, _ = template.ParseFiles("html/show.html")
 
 var listen_address = "localhost:8080"
 var filesDirectory = "./f"
@@ -61,54 +62,58 @@ func upload(w http.ResponseWriter, r *http.Request) {
 	} else {
 		fmt.Println("successfully parsed multipart form")
 	}
-	for key, value := range r.MultipartForm.Value {
-		fmt.Printf("%s:%s", key, value)
-	}
+  randomURI := uniuri.New()
+  fmt.Printf("URI: %s\n", randomURI)
+  // Create a temp directory
+  absPath, err := pfp.Abs(filesDirectory)
+  if err != nil {
+    log.Fatal("Cannot determine absolute path for file storage: " +
+    err.Error())
+  }
+  tmpDir := fmt.Sprintf("%s/%s", absPath, randomURI)
+  err = os.Mkdir(tmpDir, os.ModePerm)
+  if err != nil {
+    log.Fatal("Cannot create temp dir for file storage: " + err.Error())
+  }
+  for _, fileHeaders := range r.MultipartForm.File {
+    for _, fileHeader := range fileHeaders {
+      file, _ := fileHeader.Open()
 
-	for _, fileHeaders := range r.MultipartForm.File {
-		for _, fileHeader := range fileHeaders {
-			//file, _ := fileHeader.Open()
-			//http://golang.org/pkg/io/ioutil/#TempDir
-			path := fmt.Sprintf("files/%s", fileHeader.Filename)
-			fmt.Println("Would save to " + path)
-			//buf, _ := ioutil.ReadAll(file)
-			//ioutil.WriteFile(path, buf, os.ModePerm)
-		}
-	}
-
-	fmt.Printf("%v\n", r.FormValue("filename"))
-	f, _, err := r.FormFile("uploaded_file")
-	if err != nil {
-		fmt.Println("Error retrieving file from multipart form: " +
-			err.Error())
-		return
-	}
-	defer f.Close()
-	t, err := ioutil.TempFile("./f", "")
-	if err != nil {
-		fmt.Println("Cannot create temp file: " + err.Error())
-		return
-	}
-	defer t.Close()
-	_, err = io.Copy(t, f)
-	if err != nil {
-		fmt.Println("Cannot copy data to file: " + err.Error())
-		return
-	}
-	http.Redirect(w, r, "/view?id="+t.Name()[6:], 302)
+      // Calculate path for file storage
+      path := fmt.Sprintf("%s/%s", tmpDir, fileHeader.Filename)
+      fmt.Printf("Saving %s\n", path)
+      buf, _ := ioutil.ReadAll(file)
+      ioutil.WriteFile(path, buf, os.ModePerm)
+    }
+  }
+  http.Redirect(w, r, "/show?id="+randomURI, 302)
 }
 
 func view(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "image")
-	http.ServeFile(w, r, "image-"+r.FormValue("id"))
+  w.Header().Set("Content-Type", "image")
+  http.ServeFile(w, r, "image-"+r.FormValue("id"))
+}
+
+func show(w http.ResponseWriter, r *http.Request) {
+  type Data struct {
+    Directory string
+    FilesURI []string
+  }
+  id := r.FormValue("id");
+  d := Data{Directory: id, FilesURI: []string{"foo", "bar", "baz"} }
+  err := showTemplate.Execute(w, d)
+  if err != nil {
+    fmt.Printf("Error executing template: %s", err.Error())
+  }
 }
 
 func main() {
-	http.HandleFunc("/", errorHandler(upload))
-	http.HandleFunc("/view", errorHandler(view))
-	checkAndCreateDir(filesDirectory)
-	fmt.Println("Starting server at " + listen_address)
-	log.Fatal(http.ListenAndServe(listen_address, nil))
-	// Simple static webserver:
-	//log.Fatal(http.ListenAndServe(":8080", http.FileServer(http.Dir("/usr/share/doc"))))
+  checkAndCreateDir(filesDirectory)
+  http.HandleFunc("/", errorHandler(upload))
+  http.HandleFunc("/view", errorHandler(view))
+  http.HandleFunc("/show", errorHandler(show))
+  fmt.Println("Starting server at " + listen_address)
+  log.Fatal(http.ListenAndServe(listen_address, nil))
+  // Simple static webserver:
+  //log.Fatal(http.ListenAndServe(":8080", http.FileServer(http.Dir("/usr/share/doc"))))
 }
